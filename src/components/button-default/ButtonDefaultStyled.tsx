5de6cffdb4a6aaccd4378bc14448173132eeb593
Edsamn <eduardomsribas@gmail.comm>
2024-06-22 13:19:37 -0300

    patch: cor do botao

--- src/components/button-default/ButtonDefaultStyled.tsx
+++ src/components/button-default/ButtonDefaultStyled.tsx
@@ -4,13 +4,14 @@ const ButtonDefaultStyled = styled.button`
   width: 140px;
   height: 60px;
   border: none;
-  background-color: ${props => props.theme.colors.primary};
-  color: ${props => props.theme.colors.quaternary};
+  background-color: ${props => props.theme.colors.quaternary};
+  color: ${props => props.theme.colors.primary};
   font-size: 1rem;
   margin: 30px;
   transition: 0.5s;
   &:hover {
     background-color: ${props => props.theme.colors.tertiary};
+    color: ${props => props.theme.colors.quaternary};
   }
 `;
 
